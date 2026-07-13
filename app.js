952d9017f5d044d71832ce5f29bfbff543a8b90f
Kokoeewqe <EkaterinaN34@outlook.com>
2026-07-13 20:18:25 +0300

    Refactor message handling and improve readability

--- app.js
+++ app.js
@@ -4,63 +4,122 @@ const socket = io("https://novachat-server2.onrender.com");
 const messages = document.getElementById("messages");
 
 
-// Добавление сообщения на экран
+
 function addMessage(data) {
 
     const div = document.createElement("div");
 
     div.className = "msg";
 
-    div.textContent = data.name + ": " + data.text;
+
+    const time = new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+
+
+    div.innerHTML = `
+
+        <b>${data.name}</b>
+
+        <br>
+
+        ${data.text}
+
+        <small style="
+            display:block;
+            margin-top:8px;
+            color:#94a3b8;
+            font-size:12px;
+        ">
+            ${time}
+        </small>
+
+    `;
+
 
     messages.appendChild(div);
 
+
     messages.scrollTop = messages.scrollHeight;
+
 }
 
 
-// Получаем историю из базы
-socket.on("history", function(history) {
 
-    history.forEach(function(message) {
+
+// Получаем историю из базы
+
+socket.on("history", (history)=>{
+
+
+    messages.innerHTML="";
+
+
+    history.forEach(message=>{
 
         addMessage(message);
 
     });
 
+
 });
 
 
-// Отправка сообщения
-function send() {
-
-    const name = document.getElementById("name").value || "Гость";
-
-    const text = document.getElementById("text").value;
 
 
-    if (text.trim() === "") {
+
+// Новое сообщение
+
+socket.on("message",(data)=>{
+
+
+    addMessage(data);
+
+
+});
+
+
+
+
+
+function send(){
+
+
+    const name =
+    document.getElementById("name").value
+    || "Гость";
+
+
+
+    const text =
+    document.getElementById("text").value;
+
+
+
+    if(text.trim()===""){
+
         return;
+
     }
 
 
-    socket.emit("message", {
 
-        name: name,
 
-        text: text
+    socket.emit("message",{
+
+
+        name:name,
+
+        text:text
+
 
     });
 
 
-    document.getElementById("text").value = "";
+
+
+    document.getElementById("text").value="";
 
 }
-
-
-// Новые сообщения
-socket.on("message", function(data) {
-
-    addMessage(data);
-
-});
